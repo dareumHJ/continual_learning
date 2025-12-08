565389e250b5a839b987750bc2e43abd136c6a9b
dareumHJ <dareum@kaist.ac.kr>
2025-12-08 21:41:07 +0900

    ADD: make additional directories

---
+++ run_experiments.py
@@ -0,0 +1,17 @@
+
+
+
+def load_method(cfg.method):
+    return
+
+def load_model_pool(cfg.modelpool):
+    return
+
+def load_task_pool(cfg.taskpool):
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
